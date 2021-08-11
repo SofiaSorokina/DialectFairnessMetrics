e44505c485ec4bf4e61485856c9e9f87706fcc9e
Sofia Sorokina <sorokin.sofia@gmail.com>
2021-08-11 15:05:52 -0700

    Added summary token files and modified some code

--- DataOrginizing.py
+++ DataOrginizing.py
@@ -58,29 +58,59 @@ def oneLine3(filename):
     oneLine = '. '.join([str(elem) for elem in clean_lines])
     f.write(oneLine)
 
+#combine all oneLine .txt dialects into one AllTweets file
+def combineData1 (file1, file2, file3):
+    filenames = [file1, file2, file3]
+
+    with open("TwitterData/run1/OneAllTweets1.txt","w") as outfile:
+        for names in filenames:
+            with open(names) as infile:
+                outfile.write(infile.read())
+        outfile.write(". ")
+
+def combineData2 (file1, file2, file3):
+    filenames = [file1, file2, file3]
+
+    with open("TwitterData/run2/OneAllTweets2.txt","w") as outfile:
+        for names in filenames:
+            with open(names) as infile:
+                outfile.write(infile.read())
+        outfile.write(". ")
+
+def combineData3 (file1, file2, file3):
+    filenames = [file1, file2, file3]
+
+    with open("TwitterData/run3/OneAllTweets3.txt","w") as outfile:
+        for names in filenames:
+            with open(names) as infile:
+                outfile.write(infile.read())
+        outfile.write(". ")
+
+#converting run1 from csv to txt
 csvTotxt("TwitterData/run1/AATweets1.csv", "TwitterData/run1/AATweets1.txt")
 oneLine1("AATweets1.txt")
-csvTotxt("TwitterData/run1/AllTweets1.csv", "TwitterData/run1/AllTweets1.txt")
-oneLine1("AllTweets1.txt")
 csvTotxt("TwitterData/run1/HispTweets1.csv", "TwitterData/run1/HispTweets1.txt")
 oneLine1("HispTweets1.txt")
 csvTotxt("TwitterData/run1/WhiteTweets1.csv", "TwitterData/run1/WhiteTweets1.txt")
 oneLine1("WhiteTweets1.txt")
 
+#converting run2 from csv to txt
 csvTotxt("TwitterData/run2/AATweets2.csv", "TwitterData/run2/AATweets2.txt")
 oneLine2("AATweets2.txt")
-csvTotxt("TwitterData/run2/AllTweets2.csv", "TwitterData/run2/AllTweets2.txt")
-oneLine2("AllTweets2.txt")
 csvTotxt("TwitterData/run2/HispTweets2.csv", "TwitterData/run2/HispTweets2.txt")
 oneLine2("HispTweets2.txt")
 csvTotxt("TwitterData/run2/WhiteTweets2.csv", "TwitterData/run2/WhiteTweets2.txt")
 oneLine2("WhiteTweets2.txt")
 
+#converting run3 from csv to txt
 csvTotxt("TwitterData/run3/AATweets3.csv", "TwitterData/run3/AATweets3.txt")
 oneLine3("AATweets3.txt")
-csvTotxt("TwitterData/run3/AllTweets3.csv", "TwitterData/run3/AllTweets3.txt")
-oneLine3("AllTweets3.txt")
 csvTotxt("TwitterData/run3/HispTweets3.csv", "TwitterData/run3/HispTweets3.txt")
 oneLine3("HispTweets3.txt")
 csvTotxt("TwitterData/run3/WhiteTweets3.csv", "TwitterData/run3/WhiteTweets3.txt")
 oneLine3("WhiteTweets3.txt")
+
+#combine corresponding files together
+combineData1("TwitterData/run1/OneAATweets1.txt", "TwitterData/run1/OneHispTweets1.txt", "TwitterData/run1/OneWhiteTweets1.txt")
+combineData2("TwitterData/run2/OneAATweets2.txt", "TwitterData/run2/OneHispTweets2.txt", "TwitterData/run2/OneWhiteTweets2.txt")
+combineData3("TwitterData/run3/OneAATweets3.txt", "TwitterData/run3/OneHispTweets3.txt", "TwitterData/run3/OneWhiteTweets3.txt")
--- LSA-Text-Summarization-master/summarization.py
+++ LSA-Text-Summarization-master/summarization.py
@@ -3,29 +3,40 @@ import nltk
 nltk.download("punkt", quiet=True)
 nltk.download("stopwords", quiet=True)
 
-from nltk.corpus import stopwords
+for x in range(1,4):
+    from nltk.corpus import stopwords
 
-source_file = "TwitterData/run3/OneAllTweets3.txt"
+    i = str(x)
+    source_file = "TwitterData/run"+i+"/OneAllTweets"+i+".txt"
+    with open(source_file, "r", encoding='utf-8') as file:
+        text = file.readlines()
 
-with open(source_file, "r", encoding='utf-8') as file:
-    text = file.readlines()
+    summarizer = LsaSummarizer()
+    stopwords = stopwords.words('english')
+    summarizer.stop_words = stopwords
+    summary = summarizer(text[0], 10)
 
+    f = open("LSA-summary/Summary"+i+".txt","w")
+    endSum = " ".join(summary)
+    f.write(endSum)
 
+    #print("====== Original text "+i+" =====")
+    #print(text)
+    #print("====== End of original text "+i+" =====\n")
 
-summarizer = LsaSummarizer()
+    print("========= Summary "+i+" =========")
+    print(endSum)
+    print("========= End of summary "+i+" =========\n")
 
-stopwords = stopwords.words('english')
-summarizer.stop_words = stopwords
-summary = summarizer(text[0], 10)
+#Devides the summanry sentences and puts them in a list format
+def summaryTokens (filename):
+    my_file = open("LSA-summary/" + filename, "r")
+    content = my_file.read()
+    content_list = content.split(". ")
+    my_file.close()
+    f = open("LSA-summary/Token" + filename,"w")
+    f.write(str(content_list))
 
-#print("====== Original text =====")
-#print(text)
-#print("====== End of original text =====")
-
-
-print("\n========= Summary =========")
-
-print(" ".join(summary))
-print("========= End of summary =========")
-
-
+summaryTokens ("Summary1.txt")
+summaryTokens ("Summary2.txt")
+summaryTokens ("Summary3.txt")

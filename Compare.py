537e78bcddab924fe802c4a3078a785786299ab1
Sofia Sorokina <sorokin.sofia@gmail.com>
2021-08-13 11:45:24 -0700

    Fixed for a littled better version of Compare

--- Compare.py
+++ Compare.py
@@ -1,3 +1,10 @@
+from collections import OrderedDict
+
+def remove_items(test_list, item):
+    # using list comprehension to perform the task
+    res = [i for i in test_list if i != item]
+    return res
+
 def compare(sumFile, aaFile, hispFile, whFile):
     summary = open(sumFile, 'r')
     sum_contents = summary.read()
@@ -25,22 +32,27 @@ def compare(sumFile, aaFile, hispFile, whFile):
 
     for x in sum_list:
         for y in aa_list:
-            if x in y:
-                #print ("'" + x + "' is from AATweets")
+            if x[:-1] in y:
+                #print (x)
                 countAA +=1
-
-    for x in sum_list:
+                remove_items(aa_list, y)
+                break
         for y in hisp_list:
-            if x in y:
-                #print ("'" + x + "' is from HispTweets")
+            if x[:-1] in y:
+                #print (x)
                 countH +=1
-
-    for x in sum_list:
+                remove_items(hisp_list, y)
+                break
         for y in white_list:
-            if x in y:
-                #print ("'" + x + "' is from WhiteTweets")
+            if x[:-1] in y:
+                #print (x)
                 countWH +=1
+                remove_items(aa_list, y)
+                break
 
+    
+    print("summary: "+str(len(sum_list)))
+    print("count: "+str(countAA+countH+countWH))
 
     print("from AA: " + str(countAA) + " or " + str(round(countAA/aa_length*100, 3)) + "% --> takes up " + str(round(countAA/len(sum_list)*100, 3)) + "% in the summary")
     print("from Hisp: " + str(countH) + " or " + str(round(countH/hisp_length*100, 3)) + "% --> takes up " + str(round(countH/len(sum_list)*100, 3)) + "% in the summary")
@@ -48,15 +60,14 @@ def compare(sumFile, aaFile, hispFile, whFile):
 
 
 print("\nSummary Organized 1")
-compare("SUMY-summaries/OrderSummary1.txt", "run1/AATweets1.txt", "run1/HispTweets1.txt", "run1/WhiteTweets1.txt")
+compare("LSA-summaries/OrderSummary1.txt", "run1/AATweets1.txt", "run1/HispTweets1.txt", "run1/WhiteTweets1.txt")
 print("\nSummary Organized 2")
-compare("SUMY-summaries/OrderSummary2.txt", "run2/AATweets2.txt", "run2/HispTweets2.txt", "run2/WhiteTweets2.txt")
-"""print("\nSummary Organized 3")
-compare("SUMY-summaries/OrderSummary3.txt", "run3/AATweets3.txt", "run3/HispTweets3.txt", "run3/WhiteTweets3.txt")
+compare("LSA-summaries/OrderSummary2.txt", "run2/AATweets2.txt", "run2/HispTweets2.txt", "run2/WhiteTweets2.txt")
+print("\nSummary Organized 3")
+compare("LSA-summaries/OrderSummary3.txt", "run3/AATweets3.txt", "run3/HispTweets3.txt", "run3/WhiteTweets3.txt")
 print("\nSummary Randomised 1")
-compare("SUMY-summaries/RandomSummary1.txt", "run1/AATweets1.txt", "run1/HispTweets1.txt", "run1/WhiteTweets1.txt")
+compare("LSA-summaries/RandomSummary1.txt", "run1/AATweets1.txt", "run1/HispTweets1.txt", "run1/WhiteTweets1.txt")
 print("\nSummary Randomised 2")
-compare("SUMY-summaries/RandomSummary2.txt", "run2/AATweets2.txt", "run2/HispTweets2.txt", "run2/WhiteTweets2.txt")
+compare("LSA-summaries/RandomSummary2.txt", "run2/AATweets2.txt", "run2/HispTweets2.txt", "run2/WhiteTweets2.txt")
 print("\nSummary Randomised 3")
-compare("SUMY-summaries/RandomSummary3.txt", "run3/AATweets3.txt", "run3/HispTweets3.txt", "run3/WhiteTweets3.txt")"""
-
+compare("LSA-summaries/RandomSummary3.txt", "run3/AATweets3.txt", "run3/HispTweets3.txt", "run3/WhiteTweets3.txt")
--- SumyPythonCode.py
+++ SumyPythonCode.py
@@ -23,12 +23,15 @@ if __name__ == "__main__":
             stemmer = Stemmer(LANGUAGE)
             summarizer = Summarizer(stemmer)
             summarizer.stop_words = get_stop_words(LANGUAGE)
+            line = ""
 
             #create a new file for summary based on tweets to be stored in
             open("SUMY-summaries/"+y+"Summary"+i+".txt","w")
             print("========= Summary "+y+" "+i+" =========")
             for sentence in summarizer(parser.document, SENTENCES_COUNT):
                 print(sentence)
-                f= open("SUMY-summaries/"+y+"Summary"+i+".txt","a")
-                f.write(str(sentence)+". ")
+                line += (str(sentence) + ". ")
             print("========= End of summary "+y+" "+i+" =========\n")
+
+            f= open("SUMY-summaries/"+y+"Summary"+i+".txt","a")
+            f.write(line[:-1])
